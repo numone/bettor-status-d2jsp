b1c76fb209f3c40d7d3895e8b1506d03e2a4ec9d
christinjhansen@gmail.com <christinjhansen@gmail.com@c0c76461-2e7b-b4c0-6066-bfb0f0eab01b>
2011-11-21 04:30:15 +0000

    continued work

--- branches/devel/BettorStatus.user.js
+++ branches/devel/BettorStatus.user.js
@@ -2,7 +2,7 @@
 // @name          Bettor Status
 // @namespace     d2jsp
 // @description   Puts the betting status of the person in their avatar
-// @version       3.0.0
+// @version       3.00
 // @include       http://forums.d2jsp.org/*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js
 // ==/UserScript==
@@ -21,9 +21,9 @@ var LIST = {}; // holds the list of everything
 function showPreferencesMenu(){
 	$('BODY').css({'overflow':'hidden'});
 	$('BODY').append('<div id="Bettor_Status_bkgrnd" style="position:absolute;top:0%;left:0%;width:100%;height:100%;background-color:black;z-index:1001;-moz-opacity:0.6;opacity:.60;filter:alpha(opacity=60)">' +
-		'</div><div id="Bettor_Status_forgrnd" style="position:absolute;top:25%;left:25%;width:50%;height:50%;padding:16px;border:16px solid #8FBEFF;background-color:#E4EBFF;z-index:1002;overflow:auto;"></div>');
+		'</div><div id="Bettor_Status_forgrnd" style="position:absolute;top:25%;left:25%;width:50%;height:50%;padding:16px;border:5px solid #8FBEFF;background-color:#E4EBFF;z-index:1002;overflow:auto;"></div>');
 	var holder = $('#Bettor_Status_forgrnd');
-	$(holder).html('<div style="font-size:12pt;"><span style="font-weight:bold;text-decoration:underline;">Bettor Status Preferences:</span><span id="Bettor_Status_close" style="float:right;cursor:pointer;color:blue;text-decoration:underline;">Close</span></div>');
+	$(holder).html('<div style="font-size:12pt;"><span style="font-weight:bold;text-decoration:underline;">Bettor Status Settings:</span><span id="Bettor_Status_close" style="float:right;cursor:pointer;color:blue;text-decoration:underline;">Close</span></div>');
 	$('#Bettor_Status_close').click(function(){
 		$('#Bettor_Status_bkgrnd').remove();
 		$('#Bettor_Status_forgrnd').remove();
@@ -31,7 +31,7 @@ function showPreferencesMenu(){
 	});
 	
 	// display type
-	$(holder).append('<span style="font-weight:bold;">Status Display:</span><input type="radio" name="dispType" value="rolled" />Rolled Up <input type="radio" name="dispType" value="seporated" />Seporated');
+	$(holder).append('<span style="font-weight:bold;">Status Display:</span> <input type="radio" name="dispType" value="rolled" />Combined <input type="radio" name="dispType" value="seporated" />Seporated');
 	$(holder).find('INPUT[name="dispType"][value="' + GM_getValue('BSDispType','rolled') + '"]').prop('checked',true);
 	$(holder).find('INPUT[name="dispType"]').click(function(){
 		GM_setValue('BSDispType',$(this).prop('value'));
@@ -55,11 +55,19 @@ function showPreferencesMenu(){
 	
 	// names last updated
 	var timeAgo = Math.round(((new Date().getTime()) - (GM_getValue('BSExpireTime') - CACHE_TIME)) / 60000);
-	$(holder).append('<br /><br /><br /><span style="font-style:italic;">List Names Last Updated ' + timeAgo + ' minutes ago.</span>');
+	if(GM_getValue('BSExpireTime') == '0'){
+		$(holder).append('<br /><br /><br /><span id="Bettor_Status_refreshSpan">Names List will reload when the page is refreshed (F5)</span>');
+	}else{
+		$(holder).append('<br /><br /><br /><span id="Bettor_Status_refreshSpan"><span style="font-style:italic;">Names List Last Updated ' + timeAgo + ' minutes ago.</span> <a href="#" id="Bettor_Status_refresh">Reload Now</a></span>');
+		$('#Bettor_Status_refresh').click(function(){
+			GM_setValue('BSExpireTime','0');
+			$('#Bettor_Status_refreshSpan').html('Will now reload when the page is refreshed (F5)');
+		});
+	}
 };
 
 function showPreferencesLink(){
-	$('BODY DIV.bar DIV.barR').append('<a href="#" id="Bettor_Status_pref">Bettor Status Preferences</a>');
+	$('BODY DIV.bar DIV.barR').append('<a href="#" id="Bettor_Status_pref">Bettor Status Settings</a>');
 	$('#Bettor_Status_pref').click(function(){
 		showPreferencesMenu();
 	});
@@ -67,8 +75,35 @@ function showPreferencesLink(){
 
 function showStatus(nameList,nameHolders){
 	for(var i=0;i<nameList.length;i++){
-		var status = LIST.sports[0].names[nameList[i].toUpperCase()] ? LIST.sports[0].names[nameList[i].toUpperCase()].status : 'unknown';
-		$(nameHolders[i]).append('<div>Bettor Status: ' + status + '</div>');
+		var resultsArray = [];
+		var results = {rank:-1};
+		for(var j=0;j<LIST.sports.length;j++){
+			var sport = LIST.sports[j];
+			if(!(GM_getValue('BSHideSpt' + sport.title,false))){
+				if(sport.names[nameList[i].toUpperCase()]){
+					resultsArray.push({sport:sport.title,status:sport.names[nameList[i].toUpperCase()].status});
+					if(results.rank < LIST.statusInfo[sport.names[nameList[i].toUpperCase()].status].rank){
+						results = {rank:LIST.statusInfo[sport.names[nameList[i].toUpperCase()].status].rank,status:sport.names[nameList[i].toUpperCase()].status};
+					}
+				}else{
+					resultsArray.push({sport:sport.title,status:'unknown'});
+					if(results.rank < 0){
+						results = {rank:0,status:'unknown'};
+					}
+				}
+			}
+		}
+		if(results.rank > -1){
+			if(GM_getValue('BSDispType','rolled') == 'rolled'){
+				$(nameHolders[i]).append('<div>Bettor Status: ' + results.status + '</div>');
+			}else{
+				var appendStr = '';
+				for(var j=0;j<resultsArray.length;j++){
+					appendStr += '<div>' + resultsArray[j].sport + ': ' + resultsArray[j].status + '</div>';
+				}
+				$(nameHolders[i]).append(appendStr);
+			}
+		}
 	}
 };
 
