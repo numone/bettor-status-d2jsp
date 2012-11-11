7c84986d21c99d9eb7e25ac7e11152d65eb5aa4d
christinjhansen <christinjhansen@gmail.com>
2012-11-11 05:58:19 +0000

    poker status

--- poker/chrome/PokerStatus.js
+++ poker/chrome/PokerStatus.js
@@ -1,23 +1,18 @@
-// ==UserScript==
-// @name          Poker Status
-// @namespace     d2jsp
-// @description   Puts the betting status of the person in their avatar
-// @include       http://forums.d2jsp.org/*
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js
-// ==/UserScript==
 /**************************************
 ** COPYRIGHT NUMONE@D2jsp.org ******
 **************************************/
 //globals
-var VERSION = 1.00;
-var CACHE_TIME = 1000 * 60 * 60; //1000 to convert to seconds, 60 to convert to min, 60 for 60 minutes
-var HEADER_URL = 'http://bettor-status-d2jsp.googlecode.com/svn/lists/masterPoker.json';
-var PAGE_TYPE = window.location.href.match(/\/topic\.php/) ? 'thread'
+var POKERSTATUS = {
+	VERSION:1.03,
+	CACHE_TIME:1000 * 60 * 60, //1000 to convert to seconds, 60 to convert to min, 60 for 60 minutes
+	HEADER_URL: 'http://bettor-status-d2jsp.googlecode.com/svn/lists/masterPoker.json',
+	PAGE_TYPE : window.location.href.match(/\/topic\.php/) ? 'thread'
 	: window.location.href.match(/\/pm\.php\?c\=3/) ? 'pm'
 	: window.location.href.match(/\/user\.php/) ? 'user'
 	: window.location.href.match(/\/settings\.php/) ? 'settings'
-	: 'UNKNOWN';
-var LIST = {}; // holds the list of everything
+	: 'UNKNOWN',
+	LIST : {} // holds the list of everything
+}
 
 // universal fieldset
 $('BODY').append('<fieldset style="padding:5px;position:absolute;z-index:100;background-color:#D4E0FF;" id="psFieldset"><legend style="background-color:#D4E0FF;border:1px solid #B0B0B0;"><span></span><img style="vertical-align:inherit;margin-left:2px;cursor:pointer;" src="images/x.gif" /></legend><div class="main"></div></fieldset>');
@@ -30,7 +25,7 @@ function showPreferencesMenu(){
 	$('BODY').append('<div id="Bettor_Status_bkgrnd" style="position:absolute;top:0%;left:0%;width:100%;height:100%;background-color:black;z-index:1001;-moz-opacity:0.6;opacity:.60;filter:alpha(opacity=60)">' +
 		'</div><div id="Bettor_Status_forgrnd" style="position:absolute;top:25%;left:25%;width:50%;height:50%;padding:16px;border:5px solid #8FBEFF;background-color:#E4EBFF;z-index:1002;overflow:auto;color:black;"></div>');
 	var holder = $('#Bettor_Status_forgrnd');
-	$(holder).html('<div style="font-size:12pt;"><span style="font-weight:bold;text-decoration:underline;">Poker Status Settings:</span><span id="Bettor_Status_close" style="float:right;cursor:pointer;color:blue;text-decoration:underline;">Close</span></div>');
+	$(holder).html('<div style="font-size:12pt;"><span style="font-weight:bold;text-decoration:underline;">Bettor Status Settings:</span><span id="Bettor_Status_close" style="float:right;cursor:pointer;color:blue;text-decoration:underline;">Close</span></div>');
 	$('#Bettor_Status_close').click(function(){
 		$('#Bettor_Status_bkgrnd').remove();
 		$('#Bettor_Status_forgrnd').remove();
@@ -39,35 +34,35 @@ function showPreferencesMenu(){
 	
 	// display type
 	$(holder).append('<span style="font-weight:bold;">Status Display:</span> <input type="radio" name="dispType" value="rolled" />Combined <input type="radio" name="dispType" value="seporated" />Separated');
-	$(holder).find('INPUT[name="dispType"][value="' + GM_getValue('BSDispType','rolled') + '"]').prop('checked',true);
+	$(holder).find('INPUT[name="dispType"][value="' + (localStorage['BSDispType'] || 'rolled') + '"]').prop('checked',true);
 	$(holder).find('INPUT[name="dispType"]').click(function(){
-		GM_setValue('BSDispType',$(this).prop('value'));
+		localStorage['BSDispType'] = $(this).prop('value');
 	});
 	
 	// use list checkboxes
 	$(holder).append('<div style="margin-top:15px;font-weight:bold;">Use Status From The Following Sports:</div>');
-	for(var i=0;i<LIST.sports.length;i++){
-		$(holder).append('<input type="checkbox" name="useSport" value="' + LIST.sports[i].title + '" /> ' + LIST.sports[i].title + '<br />');
-		if(!(GM_getValue('BSHideSpt' + LIST.sports[i].title,false))){
-			$(holder).find('INPUT[type="checkbox"][value="' + LIST.sports[i].title + '"]').prop('checked',true);
+	for(var i=0;i<POKERSTATUS.LIST.sports.length;i++){
+		$(holder).append('<input type="checkbox" name="useSport" value="' + POKERSTATUS.LIST.sports[i].title + '" /> ' + POKERSTATUS.LIST.sports[i].title + '<br />');
+		if(localStorage['BSHideSpt' + POKERSTATUS.LIST.sports[i].title] != 'true'){
+			$(holder).find('INPUT[type="checkbox"][value="' + POKERSTATUS.LIST.sports[i].title + '"]').prop('checked',true);
 		}
 	}
 	$(holder).find('INPUT[type="checkbox"][name="useSport"]').click(function(){
 		if($(this).prop('checked')){
-			GM_setValue('BSHideSpt' + $(this).prop('value'),false);
+			localStorage['BSHideSpt' + $(this).prop('value')] = false;
 		}else{
-			GM_setValue('BSHideSpt' + $(this).prop('value'),true);
+			localStorage['BSHideSpt' + $(this).prop('value')] = true;
 		}
 	});
 	
 	// names last updated
-	var timeAgo = Math.round(((new Date().getTime()) - (GM_getValue('PSExpireTime') - CACHE_TIME)) / 60000);
-	if(GM_getValue('PSExpireTime') == '0'){
+	var timeAgo = Math.round(((new Date().getTime()) - (localStorage['PSExpireTime'] - POKERSTATUS.CACHE_TIME)) / 60000);
+	if(localStorage['PSExpireTime'] == '0'){
 		$(holder).append('<br /><br /><br /><span id="Bettor_Status_refreshSpan">Names List will reload when the page is refreshed (F5)</span>');
 	}else{
 		$(holder).append('<br /><br /><br /><span id="Bettor_Status_refreshSpan"><span style="font-style:italic;">Names List Last Updated ' + timeAgo + ' minutes ago.</span> <a href="#" id="Bettor_Status_refresh">Reload Now</a></span>');
 		$('#Bettor_Status_refresh').click(function(){
-			GM_setValue('PSExpireTime','0');
+			localStorage['PSExpireTime']  = '0';
 			$('#Bettor_Status_refreshSpan').html('Will now reload when the page is refreshed (F5)');
 		});
 	}
@@ -86,13 +81,13 @@ function showStatus(nameList,nameHolders){
 	for(var i=0;i<nameList.length;i++){
 		var resultsArray = [];
 		var results = {rank:-1};
-		for(var j=0;j<LIST.sports.length;j++){
-			var sport = LIST.sports[j];
-			//if(!(GM_getValue('BSHideSpt' + sport.title,false))){
+		for(var j=0;j<POKERSTATUS.LIST.sports.length;j++){
+			var sport = POKERSTATUS.LIST.sports[j];
+			//if(localStorage['BSHideSpt' + sport.title] != 'true'){
 				if(sport.names[nameList[i].toUpperCase()]){
 					resultsArray.push({sport:sport.title,status:sport.names[nameList[i].toUpperCase()].status,id:j});
-					if(results.rank < LIST.statusInfo[sport.names[nameList[i].toUpperCase()].status].rank){
-						results = {rank:LIST.statusInfo[sport.names[nameList[i].toUpperCase()].status].rank,status:sport.names[nameList[i].toUpperCase()].status};
+					if(results.rank < POKERSTATUS.LIST.statusInfo[sport.names[nameList[i].toUpperCase()].status].rank){
+						results = {rank:POKERSTATUS.LIST.statusInfo[sport.names[nameList[i].toUpperCase()].status].rank,status:sport.names[nameList[i].toUpperCase()].status};
 					}
 				}else{
 					resultsArray.push({sport:sport.title,status:'Unknown',id:j});
@@ -103,7 +98,7 @@ function showStatus(nameList,nameHolders){
 			//}
 		}
 		if(results.rank > -1){
-			// if(GM_getValue('BSDispType','rolled') == 'rolled'){
+			// if(!localStorage['BSDispType'] || localStorage['BSDispType'] == 'rolled'){
 				// function createLink(){
 					// $(nameHolders[i]).append('<div class="sportStatusHolder"><a href="javascript:void(0);">Bettor Status</a>: ' + colorTheStatus(results.status) + '</div>');
 					// var resArray = resultsArray;
@@ -114,15 +109,15 @@ function showStatus(nameList,nameHolders){
 				// createLink();
 			// }else{
 				for(var j=0;j<resultsArray.length;j++){
-					function createLinkAll(){
-						var appendStr = '<div class="sportStatusHolder"><a href="javascript:void(0);">' + resultsArray[j].sport + '</a>: ' + colorTheStatus(resultsArray[j].status) + '</div>';
+					function createLinks(idx){
+						var appendStr = '<div class="sportStatusHolder"><a href="javascript:void(0);">' + resultsArray[idx].sport + '</a>: ' + colorTheStatus(resultsArray[idx].status) + '</div>';
 						$(nameHolders[i]).append(appendStr);
-						var theID = resultsArray[j].id;
+						var theID = resultsArray[idx].id;
 						$(nameHolders[i]).find('DIV.sportStatusHolder:last A').click(function(){
 							showSingleSportInfo(theID,this);
 						});
 					}
-					createLinkAll();
+					createLinks(j);
 				}
 			//}
 		}
@@ -131,7 +126,7 @@ function showStatus(nameList,nameHolders){
 
 function showSingleSportInfo(id,link){
 	var offset = $(link).offset();
-	var sportInfo = LIST.sports[id];
+	var sportInfo = POKERSTATUS.LIST.sports[id];
 	$('#psFieldset').css({top:offset.top,left:offset.left}).show();
 	$('#psFieldset LEGEND SPAN').html(sportInfo.title + ' Info');
 	$('#psFieldset DIV.main').html('<a href="' + sportInfo.list + '" target="_blank">Link To ' + sportInfo.title + ' List Image</a><br /><br />List Ran By:<br />');
@@ -147,17 +142,17 @@ function showMultiSportsInfo(resultsArray,link){
 	$('#psFieldset DIV.main').html('');
 	for(var i=0;i<resultsArray.length;i++){
 		var t = resultsArray[i];
-		$('#psFieldset DIV.main').append('<div><a title="links to ' + t.sport + ' list" href="' + LIST.sports[t.id].list + '" target="_blank">' + t.sport + '</a>: ' + colorTheStatus(t.status) + ' <span style="font-size:7pt;">Ran By: </span></div>');
-		for(var j=0;j<LIST.sports[t.id].listRunner.length;j++){
-			var o = LIST.sports[t.id].listRunner[j];
+		$('#psFieldset DIV.main').append('<div><a title="links to ' + t.sport + ' list" href="' + POKERSTATUS.LIST.sports[t.id].list + '" target="_blank">' + t.sport + '</a>: ' + colorTheStatus(t.status) + ' <span style="font-size:7pt;">Ran By: </span></div>');
+		for(var j=0;j<POKERSTATUS.LIST.sports[t.id].listRunner.length;j++){
+			var o = POKERSTATUS.LIST.sports[t.id].listRunner[j];
 			$('#psFieldset DIV.main DIV:last SPAN:last').append(' <a href="' + o.link + '" target="_blank">' + o.name + '</a>');
 		}
 	}
 };
 
 function colorTheStatus(status){
-	if(LIST.statusInfo[status]){
-		return '<span style="color:' + LIST.statusInfo[status].color + ';font-weight:bold;">' + status + '</span>';
+	if(POKERSTATUS.LIST.statusInfo[status]){
+		return '<span style="color:' + POKERSTATUS.LIST.statusInfo[status].color + ';font-weight:bold;">' + status + '</span>';
 	}else{
 		return '<span style="color:gray;font-weight:bold;">' + status + '</span>';
 	}
@@ -166,12 +161,12 @@ function colorTheStatus(status){
 function showMedList(){
 	var forumID = parseInt(window.location.href.split('&f=')[1]);
 	var theSport = false;
-	for(var i=0;i<LIST.sports.length;i++){
-		if(LIST.sports[i].medForumIDs){
-			var medList = LIST.sports[i].medForumIDs;
+	for(var i=0;i<POKERSTATUS.LIST.sports.length;i++){
+		if(POKERSTATUS.LIST.sports[i].medForumIDs){
+			var medList = POKERSTATUS.LIST.sports[i].medForumIDs;
 			for(var j=0;j<medList.length;j++){
 				if(medList[j] == forumID){
-					theSport = LIST.sports[i];
+					theSport = POKERSTATUS.LIST.sports[i];
 					break;
 				}
 			}
@@ -204,14 +199,16 @@ function showMediators(sportObj,link){
 
 function parsePage(){
 	var names = [],nameHolders = [];
-	switch(PAGE_TYPE){
+	switch(POKERSTATUS.PAGE_TYPE){
 		case 'thread':
 			$('BODY DIV.tbb FORM[name="REPLIER"] DL').has('TABLE.ftb').find('DT A[href^="user.php"]').each(function(){
 				names.push($(this).text());
 			});
+			console.log(names);
 			$('BODY DIV.tbb FORM[name="REPLIER"] DL DD TABLE.ftb TBODY TR TD.bc1').each(function(){
 				nameHolders.push(this);
 			});
+			console.log(nameHolders.length);
 			break;
 		case 'pm':
 			$('BODY FORM[name="a"] TABLE:eq(0) TR TD DL DT A[href^="user.php"]').each(function(){
@@ -232,7 +229,7 @@ function parsePage(){
 			});
 			break;
 		case 'settings':
-			//showPreferencesLink(); //Poker shouldn't need any of these
+			//showPreferencesLink();
 			break;
 	}
 
@@ -240,7 +237,7 @@ function parsePage(){
 		showStatus(names,nameHolders);
 	}
 	
-	if(PAGE_TYPE == 'thread'){
+	if(POKERSTATUS.PAGE_TYPE == 'thread'){
 		showMedList();
 	}
 	
@@ -248,40 +245,34 @@ function parsePage(){
 };
 
 function showFlushLink(){
-	var timeAgo = Math.round(((new Date().getTime()) - (GM_getValue('PSExpireTime') - CACHE_TIME)) / 60000);
-	$('BODY DIV.crt.links').append(' | Poker Status version: ' + VERSION.toFixed(2) + ', <span id="PSBottomSpan">cached for ' + timeAgo + ' minutes <a href="javascript:void(0);" id="PSBottomFlush">flush</a></span>');
+	var timeAgo = Math.round(((new Date().getTime()) - (localStorage['PSExpireTime'] - POKERSTATUS.CACHE_TIME)) / 60000);
+	$('BODY DIV.crt.links').append(' | Poker Status version: ' + POKERSTATUS.VERSION.toFixed(2) + ', <span id="PSBottomSpan">cached for ' + timeAgo + ' minutes <a href="javascript:void(0);" id="PSBottomFlush">flush</a></span>');
 	$('#PSBottomFlush').click(function(){
-		GM_setValue('PSExpireTime','0');
+		localStorage['PSExpireTime']  = '0';
 		$('#PSBottomSpan').html('Will now reload when the page is refreshed (F5)');
 	});
 };
 
 function gatherSport(sequence){
-	if(sequence + 1 > LIST.sports.length){
+	if(sequence + 1 > POKERSTATUS.LIST.sports.length){
 		//save to cache
-		GM_setValue('PSExpireTime',(new Date().getTime() + CACHE_TIME) + '');
-		GM_setValue('PSListInfo',JSON.stringify(LIST));
+		localStorage['PSExpireTime'] = (new Date().getTime() + POKERSTATUS.CACHE_TIME) + '';
+		localStorage['PSListInfo'] = JSON.stringify(POKERSTATUS.LIST);
 		
 		parsePage();
 		return;
 	}
-	LIST.sports[sequence].names = {};
-	LIST.sports[sequence].medList = [];
+	POKERSTATUS.LIST.sports[sequence].names = {};
+	POKERSTATUS.LIST.sports[sequence].medList = [];
 	
-	GM_xmlhttpRequest({
-		method:'GET',
-		url:LIST.sports[sequence].namesURL + '?' + (new Date().getTime()),
-		headers: {
-	        'User-agent': 'Mozilla/4.0 (compatible) Greasemonkey',
-	        'Accept': 'application/atom+xml,application/xml,text/xml',
-	    },
-		onload:function(response){
-			parseNames(sequence,response.responseText);
-			gatherSport(sequence + 1);
-		},
-		onerror:function(err){
-			showErrorMsg('Error retrieving list ' + LIST.sports[sequence].title);
+	chrome.extension.sendRequest({url:POKERSTATUS.LIST.sports[sequence].namesURL + '?' + (new Date().getTime())},function(response){
+		if(!response){
+			showErrorMsg('Error retrieving list ' + POKERSTATUS.LIST.sports[sequence].title);
+			return;
 		}
+		
+		parseNames(sequence,response);
+		gatherSport(sequence + 1);
 	});
 };
 
@@ -298,15 +289,15 @@ function parseNames(sequence,responseText){
 			if(temp.match(/[^\s]\s+\/\/[0-9]+/)){
 				var temp2 = temp.split(/\s+\/\//);
 				//console.log('Name: ' + temp2[0].replace(/\/\//g,'').trim() + ' with number: ' + temp2[1].trim());
-				LIST.sports[sequence].names[temp2[0].replace(/\/\//g,'').trim().toUpperCase()] = {status:userStatus,id:temp2[1].trim()};
+				POKERSTATUS.LIST.sports[sequence].names[temp2[0].replace(/\/\//g,'').trim().toUpperCase()] = {status:userStatus,id:temp2[1].trim()};
 				if(userStatus == 'Mediator'){
-					LIST.sports[sequence].medList.push({name: temp2[0].replace(/\/\//g,'').trim(), id: temp2[1].trim()});
+					POKERSTATUS.LIST.sports[sequence].medList.push({name: temp2[0].replace(/\/\//g,'').trim(), id: temp2[1].trim()});
 				}
 			}else{
 				//console.log('Name: ' + temp.trim() + ' length: ' + temp.trim().length);
-				LIST.sports[sequence].names[temp.trim().toUpperCase()] = {status:userStatus};
+				POKERSTATUS.LIST.sports[sequence].names[temp.trim().toUpperCase()] = {status:userStatus};
 				if(userStatus == 'Mediator'){
-					LIST.sports[sequence].medList.push({name: temp.trim()});
+					POKERSTATUS.LIST.sports[sequence].medList.push({name: temp.trim()});
 				}
 			}
 		}else{
@@ -332,7 +323,7 @@ function showErrorMsg(msg){
 };
 
 function retrieveCache(){//return false;
-	var expTime = GM_getValue('PSExpireTime');
+	var expTime = localStorage['PSExpireTime'];
 	if(!expTime){//first load?
 		return false;
 	}
@@ -341,33 +332,23 @@ function retrieveCache(){//return false;
 		return false;
 	}
 	
-	LIST = JSON.parse(GM_getValue('PSListInfo'));
+	POKERSTATUS.LIST = JSON.parse(localStorage['PSListInfo']);
 	parsePage();
 	return true;
 };
 
 function retrieveHeader(){
-	GM_xmlhttpRequest({
-		method:'GET',
-		url:HEADER_URL + '?' + (new Date().getTime()),
-		headers: {
-	        'User-agent': 'Mozilla/4.0 (compatible) Greasemonkey',
-	        'Accept': 'application/atom+xml,application/xml,text/xml',
-	    },
-		onload:function(response){
-			LIST = JSON.parse(response.responseText);
-			if(LIST.version != VERSION){
-				showErrorMsg('Your poker status script is out of date, <a href="http://userscripts.org/scripts/show/152209" target="_blank">Click Here</a> to upgrade!');
-			}
-			gatherSport(0);
-		},
-		onerror:function(err){
+	chrome.extension.sendRequest({url:POKERSTATUS.HEADER_URL + '?' + (new Date().getTime())},function(response){
+		if(!response){
 			showErrorMsg('Error retrieving list header. Will used cached lists if available.');
-			if(GM_getValue('PSListInfo')){// we have old lists, we will use that for now
-				LIST = JSON.parse(GM_getValue('PSListInfo'));
+			if(localStorage['PSListInfo']){// we have old lists, we will use that for now
+				POKERSTATUS.LIST = JSON.parse(localStorage['PSListInfo']);
 				parsePage();
 			}
+			return;
 		}
+		POKERSTATUS.LIST = JSON.parse(response);
+		gatherSport(0);
 	});
 };
 
